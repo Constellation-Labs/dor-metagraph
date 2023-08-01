dd7ed0e22276990fe0642bf36840fb5db254b98e
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-08-01 15:47:20 -0300

    Adding more combiners test

--- metagraph/modules/shared_data/src/main/scala/com/my/dor_metagraph/shared_data/Combiners.scala
+++ metagraph/modules/shared_data/src/main/scala/com/my/dor_metagraph/shared_data/Combiners.scala
@@ -4,41 +4,44 @@ import cats.effect.IO
 import Data.{CheckInRef, DeviceCheckInFormatted, DeviceCheckInRaw, DeviceInfo, DeviceUpdate, FootTraffic, State}
 import cats.implicits.catsSyntaxApplicativeId
 import com.my.dor_metagraph.shared_data.Bounties.{CommercialLocationBounty, UnitDeployedBounty}
-import com.my.dor_metagraph.shared_data.DorApi.fetchDeviceInfo
+import com.my.dor_metagraph.shared_data.DorApi.{DeviceInfoAPIResponse, fetchDeviceInfo}
 import com.my.dor_metagraph.shared_data.Utils.customUpdateSerialization
-import io.bullet.borer.Borer.Error.InvalidInputData
 import io.bullet.borer.Cbor
 import monocle.Monocle.toAppliedFocusOps
 import org.tessellation.currency.dataApplication.L0NodeContext
-import org.tessellation.schema.SnapshotOrdinal
 import org.tessellation.schema.address.Address
 import org.tessellation.security.SecurityProvider
 import org.tessellation.security.signature.Signed
 import org.tessellation.security.hash.Hash
 
 object Combiners {
-  def combine(acc: State, address: Address, deviceCheckIn: DeviceCheckInRaw, publicKey: String, snapshotOrdinal: SnapshotOrdinal, checkInHash: String, epochProgress: Long): State = {
+  def combine(
+               acc: State,
+               address: Address,
+               deviceCheckIn: DeviceCheckInRaw,
+               publicKey: String,
+               snapshotOrdinal: Long,
+               checkInHash: String,
+               epochProgress: Long,
+               deviceInfo: DeviceInfoAPIResponse
+             ): State = {
     val state = acc.devices.get(address)
-    fetchDeviceInfo(publicKey) match {
-      case Some(deviceInfo) =>
-        val footTraffics = deviceCheckIn.e.map { event => FootTraffic(event.head, event.last) }
-        val checkInRef = CheckInRef(snapshotOrdinal.value.value, checkInHash)
-        val checkInFormatted = DeviceCheckInFormatted(deviceCheckIn.ac, deviceCheckIn.dts, footTraffics, checkInRef)
+    val footTraffics = deviceCheckIn.e.map { event => FootTraffic(event.head, event.last) }
+    val checkInRef = CheckInRef(snapshotOrdinal, checkInHash)
+    val checkInFormatted = DeviceCheckInFormatted(deviceCheckIn.ac, deviceCheckIn.dts, footTraffics, checkInRef)
 
-        val newState = state match {
-          case Some(current) =>
-            DeviceInfo(checkInFormatted, publicKey, current.bounties, deviceInfo, epochProgress)
-          case None =>
-            val bounties = List(
-              UnitDeployedBounty("UnitDeployed"),
-              CommercialLocationBounty("CommercialLocation")
-            )
-            DeviceInfo(checkInFormatted, publicKey, bounties, deviceInfo, epochProgress)
-        }
-
-        acc.focus(_.devices).modify(_.updated(address, newState))
-      case None => acc
+    val newState = state match {
+      case Some(current) =>
+        DeviceInfo(checkInFormatted, publicKey, current.bounties, deviceInfo, epochProgress)
+      case None =>
+        val bounties = List(
+          UnitDeployedBounty("UnitDeployed"),
+          CommercialLocationBounty("CommercialLocation")
+        )
+        DeviceInfo(checkInFormatted, publicKey, bounties, deviceInfo, epochProgress)
     }
+
+    acc.focus(_.devices).modify(_.updated(address, newState))
   }
 
   def getDeviceCheckInFromCBOR(data: String): DeviceCheckInRaw = {
@@ -52,7 +55,7 @@ object Combiners {
     }
   }
 
-  def getCheckInHash(update: DeviceUpdate) = Hash.fromBytes(customUpdateSerialization(update)).toString
+  def getCheckInHash(update: DeviceUpdate): String = Hash.fromBytes(customUpdateSerialization(update)).toString
 
   def combineDeviceCheckIn(acc: State, signedUpdate: Signed[DeviceUpdate])(implicit context: L0NodeContext[IO]): IO[State] = {
     try {
@@ -64,12 +67,14 @@ object Combiners {
       val ordinalIO = context.getLastCurrencySnapshot.map(_.get.ordinal)
       val epochProgress = 1440L // Should be replaced
       val addressIO = signedUpdate.proofs.map(_.id).head.toAddress[IO]
-
-      for {
-        ordinal <- ordinalIO
-        address <- addressIO
-      } yield
-        combine(acc, address, deviceCheckIn, publicKey, ordinal, checkInHash, epochProgress)
+      fetchDeviceInfo(publicKey) match {
+        case Some(deviceInfo) =>
+          for {
+            ordinal <- ordinalIO
+            address <- addressIO
+          } yield combine(acc, address, deviceCheckIn, publicKey, ordinal.value.value, checkInHash, epochProgress, deviceInfo)
+        case None => acc.pure[IO]
+      }
     } catch {
       case e: Exception =>
         println(e.getMessage)
--- metagraph/modules/shared_data/src/test/scala/com/my/dor_metagraph/shared_data/CombinersTest.scala
+++ metagraph/modules/shared_data/src/test/scala/com/my/dor_metagraph/shared_data/CombinersTest.scala
@@ -1,9 +1,13 @@
 package com.my.dor_metagraph.shared_data
 
 import cats.effect.IO
-import com.my.dor_metagraph.shared_data.Combiners.getDeviceCheckInFromCBOR
-import com.my.dor_metagraph.shared_data.Data.DeviceCheckInRaw
+import com.my.dor_metagraph.shared_data.Bounties.{CommercialLocationBounty, UnitDeployedBounty}
+import com.my.dor_metagraph.shared_data.Combiners.{combine, getCheckInHash, getDeviceCheckInFromCBOR}
+import com.my.dor_metagraph.shared_data.Data.{CheckInRef, DeviceCheckInFormatted, DeviceCheckInRaw, DeviceInfo, DeviceUpdate, FootTraffic, State}
+import com.my.dor_metagraph.shared_data.DorApi.DeviceInfoAPIResponse
+import org.tessellation.schema.address.Address
 import weaver.SimpleIOSuite
+
 object CombinersTest extends SimpleIOSuite {
 
   pureTest("Get correctly device check in from CBOR ") {
@@ -11,8 +15,8 @@ object CombinersTest extends SimpleIOSuite {
     val checkIn = getDeviceCheckInFromCBOR(cborString)
 
     expect.eql(3, checkIn.ac.size) &&
-      expect.eql(143, checkIn.ac.head ) &&
-      expect.eql(-181, checkIn.ac(1) ) &&
+      expect.eql(143, checkIn.ac.head) &&
+      expect.eql(-181, checkIn.ac(1)) &&
       expect.eql(-9657, checkIn.ac.last) &&
       expect.eql(1669815076, checkIn.dts) &&
       expect.eql(10, checkIn.e.size) &&
@@ -26,6 +30,76 @@ object CombinersTest extends SimpleIOSuite {
     } yield {
       val errorMessage = error.left.map(_.getMessage())
       expect(errorMessage == Left("Expected Long but got Array Header (15) (input position 7)"))
+    }
+  }
+
+  pureTest("Get check in hash successfully") {
+    val cborString = "A362616383188F38B43925B8636474731A63875B2461658A821B00000184A0C9AF5E01821B00000194A0CD649601821B00000184A0CE04BF01821B00000184A0D0CF9801821B00000184A0D3254101821B00000184A0D3968A01821B00000184A0D3C95301821B00000184A0D3F06401821B00000184A0D47D0501821B00000184A0D48CA601"
+    val checkIn = getCheckInHash(DeviceUpdate(cborString))
+
+    expect.eql("99dc7d3f955a5edef7356e25a46bcb9ce9cd1bee7084832bf70c2903cd9cf6d0", checkIn)
+  }
+
+  pureTest("Create a new check in on state") {
+    val oldState = State(Map.empty)
+    val address = Address.fromBytes("DAG0DQPuvVThrHnz66S4V6cocrtpg59oesAWyRMb".getBytes)
+    val checkInRaw = DeviceCheckInRaw(List(1, 2, 3), 123456, List(List(12345, 1), List(6789, -1)))
+    val publicKey = "22f97a140b17556e582efa667489bc0097eae9349707790630b0d5133d3b01db67ce0e6adb65e154096036ce5619b5fbdd116dbb07c12f9c68c2306f9830dbc6"
+    val snapshotOrdinal = 10L
+    val checkInHash = "e12345"
+    val epochProgress = 1440L
+    val deviceInfoAPIResponse = DeviceInfoAPIResponse(address, linkedToStore = true, Some("Retail"))
+
+    val allCheckIns = combine(oldState, address, checkInRaw, publicKey, snapshotOrdinal, checkInHash, epochProgress, deviceInfoAPIResponse)
+
+    allCheckIns.devices.get(address) match {
+      case Some(checkIn) =>
+        expect.eql(publicKey, checkIn.publicKey) &&
+          expect.eql(epochProgress, checkIn.lastCheckInEpochProgress) &&
+          expect.eql(checkInHash, checkIn.lastCheckIn.checkInRef.hash) &&
+          expect.eql(snapshotOrdinal, checkIn.lastCheckIn.checkInRef.ordinal) &&
+          expect.eql(checkInRaw.dts, checkIn.lastCheckIn.dts) &&
+          expect.eql(checkInRaw.ac, checkIn.lastCheckIn.ac) &&
+          expect.eql(checkInRaw.e.head.head, checkIn.lastCheckIn.footTraffics.head.timestamp) &&
+          expect.eql(checkInRaw.e.head.last, checkIn.lastCheckIn.footTraffics.head.direction) &&
+          expect.eql(checkInRaw.e.last.head, checkIn.lastCheckIn.footTraffics.last.timestamp) &&
+          expect.eql(checkInRaw.e.last.last, checkIn.lastCheckIn.footTraffics.last.direction)
+      case None =>
+        //forcing failure
+        expect.eql(1, 2)
+    }
+  }
+
+  pureTest("Update check in of device") {
+    val currentAddress = Address.fromBytes("DAG0DQPuvVThrHnz66S4V6cocrtpg59oesAWyRMb".getBytes)
+    val currentPublicKey = "22f97a140b17556e582efa667489bc0097eae9349707790630b0d5133d3b01db67ce0e6adb65e154096036ce5619b5fbdd116dbb07c12f9c68c2306f9830dbc6"
+    val currentBounties = List(UnitDeployedBounty("UnitDeployedBounty"), CommercialLocationBounty("CommercialLocationBounty"))
+    val currentDeviceInfoAPIResponse = DeviceInfoAPIResponse(currentAddress, linkedToStore = true, Some("Retail"))
+    val currentEpochProgress = 1440L
+    val currentSnapshotOrdinal = 10L
+    val currentCheckInHash = "e12345"
+    val currentCheckInRaw = DeviceCheckInFormatted(List(1, 2, 3), 123456, List(FootTraffic(12345, 1), FootTraffic(12345, 1)), CheckInRef(currentSnapshotOrdinal, currentCheckInHash))
+
+    val oldState = State(Map(currentAddress -> DeviceInfo(currentCheckInRaw, currentPublicKey, currentBounties, currentDeviceInfoAPIResponse, currentEpochProgress)))
+
+    val checkInRaw = DeviceCheckInRaw(List(1, 2, 3), 123456, List(List(12345, 1), List(6789, -1)))
+    val allCheckIns = combine(oldState, currentAddress, checkInRaw, currentPublicKey, 20L, "e123410", 2880L, currentDeviceInfoAPIResponse)
+
+    allCheckIns.devices.get(currentAddress) match {
+      case Some(checkIn) =>
+        expect.eql(currentPublicKey, checkIn.publicKey) &&
+          expect.eql(2880L, checkIn.lastCheckInEpochProgress) &&
+          expect.eql("e123410", checkIn.lastCheckIn.checkInRef.hash) &&
+          expect.eql(20L, checkIn.lastCheckIn.checkInRef.ordinal) &&
+          expect.eql(checkInRaw.dts, checkIn.lastCheckIn.dts) &&
+          expect.eql(checkInRaw.ac, checkIn.lastCheckIn.ac) &&
+          expect.eql(checkInRaw.e.head.head, checkIn.lastCheckIn.footTraffics.head.timestamp) &&
+          expect.eql(checkInRaw.e.head.last, checkIn.lastCheckIn.footTraffics.head.direction) &&
+          expect.eql(checkInRaw.e.last.head, checkIn.lastCheckIn.footTraffics.last.timestamp) &&
+          expect.eql(checkInRaw.e.last.last, checkIn.lastCheckIn.footTraffics.last.direction)
+      case None =>
+        //forcing failure
+        expect.eql(1, 2)
     }
   }
 }
