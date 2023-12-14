df8756c6e1cab3a52a518ccad01aa05fe785fb56
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-12-14 16:24:23 -0300

    Fixing future check=ins

--- metagraph/modules/shared_data/src/main/scala/com/my/dor_metagraph/shared_data/validations/TypeValidators.scala
+++ metagraph/modules/shared_data/src/main/scala/com/my/dor_metagraph/shared_data/validations/TypeValidators.scala
@@ -1,9 +1,13 @@
 package com.my.dor_metagraph.shared_data.validations
 
-import com.my.dor_metagraph.shared_data.Errors.{CheckInOlderThanAllowed, DeviceNotRegisteredOnDorApi, RepeatedCheckIn}
+import com.my.dor_metagraph.shared_data.Errors._
 import com.my.dor_metagraph.shared_data.types.Types.{CheckInDataCalculatedState, CheckInUpdate, MinimumCheckInSeconds}
+import eu.timepit.refined.auto._
 import org.tessellation.currency.dataApplication.dataApplication.DataApplicationValidationErrorOr
 import org.tessellation.schema.address.Address
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 object TypeValidators {
 
@@ -11,13 +15,29 @@ object TypeValidators {
     state        : CheckInDataCalculatedState,
     checkInUpdate: CheckInUpdate,
     address      : Address
-  ): DataApplicationValidationErrorOr[Unit] =
-    RepeatedCheckIn.whenA(state.devices.get(address).exists(_.lastCheckIn >= checkInUpdate.dts))
+  ): DataApplicationValidationErrorOr[Unit] = {
+    //This will be removed, we just need to bypass these addresses to fix one production bug
+    val addressesToIgnore = List[Address](
+      Address("DAG1fF3hRwWka2BD1wF8Y74B1g4AbatPbgpy7QWQ"),
+      Address("DAG0JVTM5RACzFDiDPVLn9hseU4TgqEEg4zhfvV5"),
+      Address("DAG0kfNUbnUBRuDZn5V28Jn2ub1bYaFRVMcMsz9m")
+    )
+    if (addressesToIgnore.contains(address)) {
+      valid
+    } else {
+      RepeatedCheckIn.whenA(state.devices.get(address).exists(_.lastCheckIn >= checkInUpdate.dts))
+    }
+  }
 
-  def validateCheckInLimitTimestamp(
+  def validateIfCheckInIsGreaterThanLimitTimestamp(
     checkInUpdate: CheckInUpdate
   ): DataApplicationValidationErrorOr[Unit] =
     CheckInOlderThanAllowed.whenA(MinimumCheckInSeconds > checkInUpdate.dts)
+
+  def validateIfCheckInIsLowerThanOneDayFromCurrentDate(
+    checkInUpdate: CheckInUpdate
+  ): DataApplicationValidationErrorOr[Unit] =
+    FutureCheckInNotAllowed.whenA(Instant.now.plus(1, ChronoUnit.DAYS).toEpochMilli / 1000L < checkInUpdate.dts)
 
   def validateIfDeviceIsRegisteredOnDORApi(
     checkInUpdate: CheckInUpdate
